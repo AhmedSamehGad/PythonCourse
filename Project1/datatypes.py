8bdc7a89a3cf352fd74b1cb4690c7f9eae0c869c
AhmedSamehGad <ahmad.sameh.gad@gmail.com>
2025-01-24 18:01:26 +0200

    Add files via upload

---
+++ Project1/datatypes.py
@@ -0,0 +1,17 @@
+#string datatype 
+
+print('Hello world')
+print('Hello ' + 'Ahmed')
+
+# integer datatype 
+
+print(5 + 10)
+
+# float datatype
+
+print(10.5 + 10)
+
+# bool datatype
+
+print(True)
+print(False)
---
+++ Project1/vars.py
@@ -0,0 +1,14 @@
+# e.g.
+
+name = 'ahmed'
+age = 18
+_isMaleMale = True
+
+print(age)
+
+# print( "Hello"+ " " + name)
+
+name = "samy"
+age += 1
+print(name)
+print(age)
